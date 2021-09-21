df62bc6afe06ea0e96aacb779ea2f227ba7356a0
Joan Gil <joan.gil@convidad.es>
2021-09-21 20:26:27 +0200

    Added initial project with xamarin essentials and dependency injection

---
+++ WeatherOnRoute/WeatherOnRoute/WeatherOnRoute/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System;
+
+using Xamarin.Forms;
+
+namespace WeatherOnRoute
+{
+    public partial class App : Application
+    {
+        protected static IServiceProvider ServiceProvider { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+            SetupServices();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        void SetupServices()
+        {
+            var services = new ServiceCollection();
+
+            // Add Services or ViewModels
+            // services.AddSingleton<IDataService, SampleDataService>();
+
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+    }
+}
---
+++ WeatherOnRoute/WeatherOnRoute/WeatherOnRoute/Services/WeatherService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherOnRoute.Services
+{
+    internal class WeatherService
+    {
+    }
+}
